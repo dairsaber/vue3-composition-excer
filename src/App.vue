67b6052f0130d8c9b53139d8117466dd3cf984d5
panafeng <pan20080808@qq.com>
2020-09-21 22:05:39 +0800

    定位

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <MenuList />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import MenuList from "./components/MenuList.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
     MenuList,
   },
 };
--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,6 +1,16 @@
 <template>
+  <header>{{ currentActiveChild.title }}</header>
   <div class="container" ref="containerRef">
-    <div v-for="item in dataSource" :key="item.title" :ref="setItemRef">
+    <div
+      class="item"
+      v-for="item in dataSource"
+      :key="item.title"
+      :ref="
+        (el) => {
+          setItemRef(el, item);
+        }
+      "
+    >
       <h3>{{ item.title }}</h3>
       <div class="children-container">
         <p v-for="child in item.children" :key="child">{{ child }}</p>
@@ -22,19 +32,31 @@ import {
   watch,
   nextTick,
   onUnmounted,
+  computed,
 } from "vue";
-
+import { Debounce } from "../utils/streamControl";
 import { getDataSource } from "./data";
+const myDebounce = new Debounce();
 export default defineComponent({
   setup(props) {
     const containerRef = ref(null);
-    let itemRefs = ref([]);
-    const setItemRef = (el) => {
-      itemRefs.value.push(el);
+    const itemRefs = ref([]);
+    const setItemRef = (el, item) => {
+      itemRefs.value.push({ el, item });
     };
     const state = reactive({
       dataSource: [],
     });
+    const activeChild = useScroll(containerRef, itemRefs);
+    const currentActiveChild = computed(
+      () => activeChild.value || { title: "加载中..." }
+    );
+    watch(
+      () => activeChild.value,
+      (val) => {
+        console.log(val);
+      }
+    );
     onBeforeUpdate(() => {
       itemRefs.value = [];
     });
@@ -43,25 +65,32 @@ export default defineComponent({
         state.dataSource = dataSource;
       });
     });
-    useScroll(containerRef, itemRefs);
-    watch(
-      () => state.dataSource,
-      () => {
-        nextTick(() => {
-          console.log("containerRef watch:>> ", containerRef, itemRefs);
-        });
-      }
-    );
-    return { ...toRefs(state), setItemRef, containerRef };
+    return { ...toRefs(state), setItemRef, containerRef, currentActiveChild };
   },
 });
 
 function useScroll(containerRef, childrenRefs) {
+  const activeChild = ref(null);
   let container;
-  function handler(event) {
-    const children = childrenRefs.value;
-    console.log("useScroll :>> ", event);
-  }
+  const handler = (event) => {
+    //节流延迟
+    myDebounce.go(() => {
+      const children = childrenRefs.value;
+      const scrollTop = container.scrollTop;
+      const offsetHeight = container.offsetHeight;
+      const containerOffsetTop = container.offsetTop;
+      const currentActivedItem = children.find((child) => {
+        const childOffsetParentTop = child.el.offsetTop - containerOffsetTop;
+        return (
+          childOffsetParentTop >= scrollTop - offsetHeight &&
+          childOffsetParentTop < scrollTop
+        );
+      });
+      if (currentActivedItem) {
+        activeChild.value = currentActivedItem.item;
+      }
+    }, 80);
+  };
   onMounted(() => {
     container = containerRef.value;
     container.addEventListener("scroll", handler);
@@ -69,14 +98,19 @@ function useScroll(containerRef, childrenRefs) {
   onUnmounted(() => {
     container.removeEventListener("scroll", handler);
   });
+  return activeChild;
 }
 </script>
 
 <style scoped>
 .container {
-  height: 800px;
+  height: 600px;
   width: 600px;
   overflow-y: auto;
+  margin: 0 auto;
+}
+.item {
+  border-top: 1px solid #888;
 }
 .children-container {
   background-color: #eee;
